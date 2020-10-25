7637ad4ed70fb7636b183a6fd223a5d5cd2121e1
archita <all.archita17@gmail.com>
2020-10-25 14:10:11 +0530

    change in app.js file

--- app.js
+++ app.js
@@ -2,17 +2,18 @@
 var express = require("express"),
   app = express(),
   mongoose = require("mongoose"),
+  routes = require("./routes/routes");
   methodOverride = require("method-override"),
   bodyParser = require("body-parser"),
   { check, validationResult } = require("express-validator"),
-  apiroutes = require("../attendance/routes/apiroutes");
+  // apiroutes = require("");
 
 // const app = express();
 // app.use(express.json());
-app.use(bodyParser.urlencoded({ extended: true }));
+// app.use(bodyParser.urlencoded({ extended: true }));
 
 // Including Routes!
-var routes = require("./routes/routes.js");
+
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
@@ -36,7 +37,7 @@ mongoose.connect(
 // var Team = require("./models/team.js");
 // var Recruit = require("./models/recruit.js");
 app.use("/", routes);
-app.use("/api/recruit", apiroutes);
+// app.use("/api/recruit", apiroutes);
 
 app.listen(5000, function() {
   console.log("Server Started!");
