dc835446c4c6b1b23cf9867e27a42b22ee21f5c3
yasharjit <yasharjitm10@gmail.com>
2020-10-25 22:08:39 +0530

    Create a newDeal route and connect it to the new deal page

--- routes/routes.js
+++ routes/routes.js
@@ -7,17 +7,17 @@ var Team = require("../models/team");
 const recruit = require("../models/recruit");
 const { check, validationResult } = require("express-validator");
 
-router.get("/attendance", function(req, res) {
+router.get("/attendance", function (req, res) {
   res.render("attendance");
 });
 
-router.post("/submit", function(req, res) {
+router.post("/submit", function (req, res) {
   //some code here
   var payas = req.body.payas;
   var sushant = req.body.sushant;
   var saksham = req.body.saksham;
   var newAtd = { Payas: payas, Sushant: sushant, Saksham: saksham };
-  Team.create(newAtd, function(err, newattendance) {
+  Team.create(newAtd, function (err, newattendance) {
     if (err) {
       console.log(err);
     } else {
@@ -29,34 +29,38 @@ router.post("/submit", function(req, res) {
   // req.flash("success","Attendance Submitted!");
 });
 
-
-router.get("/recruit",function(req,res){
-  res.render("recruit",{errors:""});
- });
-router.post("/recsubmit",[check('data[phone]',"Must be a length 0f 10").isLength({min:10,max:10})
-],function(req,res){
+router.get("/recruit", function (req, res) {
+  res.render("recruit", { errors: "" });
+});
+router.post(
+  "/recsubmit",
+  [
+    check("data[phone]", "Must be a length 0f 10").isLength({
+      min: 10,
+      max: 10,
+    }),
+  ],
+  function (req, res) {
     var errors = validationResult(req);
     console.log(errors);
-    if(!errors.isEmpty()){
-      res.render("recruit",{errors:errors.mapped()});
-    }
-    else{
+    if (!errors.isEmpty()) {
+      res.render("recruit", { errors: errors.mapped() });
+    } else {
       console.log(req.body.data);
-      recruit.create(req.body.data,function(err,newdetails){
-        if(err){
+      recruit.create(req.body.data, function (err, newdetails) {
+        if (err) {
           console.log(err);
           res.redirect("/recruit");
-        }
-        else{
+        } else {
           res.redirect("/");
         }
-      })
+      });
     }
-});
+  }
+);
 
-
-router.get("/edit", function(req, res) {
-  Team.find({}, function(err, allAttendance) {
+router.get("/edit", function (req, res) {
+  Team.find({}, function (err, allAttendance) {
     if (err) {
       console.log(err);
     } else {
@@ -65,8 +69,8 @@ router.get("/edit", function(req, res) {
   });
 });
 
-router.get("/view", function(req, res) {
-  Team.find({}, function(err, allAttendance) {
+router.get("/view", function (req, res) {
+  Team.find({}, function (err, allAttendance) {
     if (err) {
       console.log(err);
     } else {
@@ -74,16 +78,16 @@ router.get("/view", function(req, res) {
     }
   });
 });
-router.get("/attendance",function(req,res){
+router.get("/attendance", function (req, res) {
   res.render("attendance");
-})
+});
 
-router.put("/:id", async function(req, res) {
+router.put("/:id", async function (req, res) {
   console.log(req.params.id);
   const updated = await Team.findByIdAndUpdate(
     req.params.id,
     req.body,
-    function(err, updatedatt) {
+    function (err, updatedatt) {
       if (!err) {
         res.redirect("/edit");
       } else {
@@ -93,38 +97,38 @@ router.put("/:id", async function(req, res) {
   );
   console.log(updated);
 });
-router.get("/",function(req,res){
+router.get("/", function (req, res) {
   res.render("index");
 });
-router.get("/about",function(req,res){
+router.get("/about", function (req, res) {
   res.render("about");
 });
-router.get("/checkout",function(req,res){
+router.get("/checkout", function (req, res) {
   res.render("checkout");
 });
-router.get("/contact",function(req,res){
+router.get("/contact", function (req, res) {
   res.render("contact");
 });
-router.get("/help",function(req,res){
+router.get("/help", function (req, res) {
   res.render("help");
 });
-router.get("/product",function(req,res){
+router.get("/product", function (req, res) {
   res.render("product");
 });
-router.get("/product2",function(req,res){
+router.get("/product2", function (req, res) {
   res.render("product2");
 });
-router.get("/single",function(req,res){
+router.get("/single", function (req, res) {
   res.render("single");
 });
-router.get("/single2",function(req,res){
+router.get("/single2", function (req, res) {
   res.render("single2");
 });
-router.get("/TFF",function(req,res){
+router.get("/TFF", function (req, res) {
   res.render("TFF");
 });
-
-
-
+router.get("/newDeal", function (req, res) {
+  res.render("newDeal");
+});
 
 module.exports = router;
