c84a270c741c7f0729209e2005ad2b92c90ff400
yasharjit <yasharjitm10@gmail.com>
2020-11-28 22:39:09 +0530

    formatting changes

--- models/likedDealModel.js
+++ models/likedDealModel.js
@@ -1,29 +1,34 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const likedDealSchema = new mongoose.Schema({
-   deal: {
-      type: mongoose.Schema.ObjectId, ref: 'Deal',
-      required: [true, 'like Not Possible Without A Deal']
-   },
-   user: {
-      type: mongoose.Schema.ObjectId, ref: 'User',
-      required: [true, 'like Not Possible Without A User']
-   }
-}, { toJSON: { virtuals: true }, toObject: { virtuals: true } });
+const likedDealSchema = new mongoose.Schema(
+  {
+    deal: {
+      type: mongoose.Schema.ObjectId,
+      ref: "Deal",
+      required: [true, "like Not Possible Without A Deal"],
+    },
+    user: {
+      type: mongoose.Schema.ObjectId,
+      ref: "User",
+      required: [true, "like Not Possible Without A User"],
+    },
+  },
+  { toJSON: { virtuals: true }, toObject: { virtuals: true } }
+);
 
-likedDealSchema.index({ deal: 1, user: 1 }, { unique: true });
+// likedDealSchema.index({ deal: 1, user: 1 }, { unique: true });
 
 likedDealSchema.pre(/^find/, function (next) {
-   this.populate({
-      path: 'deal',
-      // select: 'name'
-   }).populate({
-      path: 'user',
-      // select: 'name'
-   });
+  this.populate({
+    path: "deal",
+    // select: 'name'
+  }).populate({
+    path: "user",
+    // select: 'name'
+  });
 
-   next();
+  next();
 });
 
-const LikedDeal = mongoose.model('LikedDeal', likedDealSchema);
+const LikedDeal = mongoose.model("LikedDeal", likedDealSchema);
 module.exports = LikedDeal;
