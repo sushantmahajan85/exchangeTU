df0be5ecc062cef0188370064adf307732068ed6
sushantmahajan85 <sushantmahajan85@gmail.com>
2020-11-30 12:03:03 +0530

    port

--- app.js
+++ app.js
@@ -51,6 +51,6 @@ app.use("/api/v1/users", userRoute);
 app.use("/api/v1/likedDeal", likedDealRoute);
 // app.use("/api/recruit", apiroutes);
 
-app.listen(5000 || process.env.PORT,'0.0.0', function () {
+app.listen(5000 || process.env.PORT, function () {
   console.log("Server Started!");
 });
